9b927d51ce7c95260d92f42274d6d5309dc9dc44
fyraas33 <cfyras@gmail.com>
2022-10-10 12:57:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {Component} from 'react';
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+
+
+class MapContainer extends Component {
+  render() {
+    return (
+<Map
+google = {this.props.google}
+style={{height: '100%', width: '100%'}}
+zoom = {10}
+initialCenter = {
+{
+  lat: 33.883333,
+  lng: 10.116667000000007
+}
+
+}>
+ 
+  <Marker/>
+ 
+</Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDTb62ngSxLXVoJQX52peiSlBczQHF6eqw'
+})(MapContainer)
